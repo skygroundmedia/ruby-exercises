47d55724eab90ff72e407e8652fbe02cae07866c
Chris Mendez <mail@chrisaiv.com>
2016-07-28 11:51:55 -0700

    refactored to make it more class basd

--- Utilities/Time.rb
+++ Utilities/Time.rb
@@ -1,17 +1,30 @@
-#Find out the Current Time
-t = Time.now
+class TimeUtil
+	@t = nil
+		
+	def initialize
+		@t= Time.now
+	end
+	
+	def mm_dd_yy
+		#Find out the Month, Day, or Year individually
+		puts @t.mon.to_s + "/" + @t.day.to_s + "/" + @t.year.to_s
 
-#Find out the Month, Day, or Year individually
-puts t.mon.to_s + "/" + t.day.to_s + "/" + t.year.to_s
+		#Another way to Display the Month Day, Year
+		puts @t.strftime("%b %d, %Y")		
+	end
+	
+	def utc
+		#Find out the UTC
+		puts @t.utc
+	end
+	
+	def show
+		#the String For Time function allows you to mix text with data
+		puts @t.strftime("The Document print data is %m/%d/%Y")
+	end
+end
 
-#Find out the UTC
-puts t.utc
-
-###########################
-#User Formatted Characters
-###########################
-#the String For Time function allows you to mix text with data
-puts t.strftime("The Document print data is %m/%d/%Y")
-
-#Another way to Display the Month Day, Year
-puts t.strftime("%b %d, %Y")
+t = TimeUtil.new
+t.mm_dd_yy
+t.utc
+t.show
