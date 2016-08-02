65468cba09509c87a721f649bf4642ba1af7ab75
Chris Mendez <mail@chrisaiv.com>
2016-08-02 06:39:36 -0700

    adding tests

---
+++ Tests/my_rails_test/spec/models/project_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+=begin
+RSpec.describe Project do
+it "considers a project with no tasks to be done" do
+    project = Project.new
+    expect(project.done?).to be_truthy
+end end=end
