c5e5707d869583851e3efc0dc14da3a74313013d
Chris Mendez <mail@chrisaiv.com>
2017-01-27 12:00:02 -0800

    Refactored some more

---
+++ R/Restroom/basic-simulation/src/simulation/config.rb
@@ -0,0 +1,24 @@
+# ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
+# Config
+#
+require 'singleton'
+require 'fileutils'
+
+class Config
+  include Singleton
+
+  APP_ROOT = File.dirname(__FILE__)  
+
+  # Directories
+  attr_reader :input_dir, :output_dir
+  # File Paths
+	attr_reader :r_path, :csv_path, :out_path
+  def initialize
+    @input_dir  = File.join(APP_ROOT, 'src')
+    @output_dir = File.join(APP_ROOT, 'bin')
+    
+    @r_path     = File.join(@input_dir, "analysis", "monte_carlo.R")
+    @csv_path   = File.join(@output_dir, "simulation.csv")
+    @out_path   = File.join(@output_dir, "simulation.out")
+  end
+end
